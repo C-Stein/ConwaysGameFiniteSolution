00329e33ab997c965cda7ff4f024f3910b6e54c9
C-Stein <caitlin.teter@gmail.com>
2015-11-10 14:14:29 -0600

    finite solution

---
+++ ConwaysGameOfLife/GameOfLife.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConwaysGameOfLife
+{
+    public class GameOfLife : Board
+    {
+        public bool[,] grid;
+        public int Row { get; set; }
+        public int Col { get; set; }
+        public GameOfLife()
+        {
+
+        }
+        public GameOfLife( int row, int col)
+        {
+            Row = row;
+            Col = col;
+            grid = new bool[row, col];
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    grid[i, j] = false;
+                }
+            }
+        }
+        public int GetAliveNeighbors(int row, int col)
+        {
+            int neighbors = 0;
+            if (grid[row - 1, col - 1])
+            {
+                neighbors++;
+            }
+            if (grid[row - 1, col])
+            {
+                neighbors++;
+            }
+            if (grid[row - 1, col + 1])
+            {
+                neighbors++;
+            }
+            if (grid[row, col - 1])
+            {
+                neighbors++;
+            }
+            if (grid[row, col + 1])
+            {
+                neighbors++;
+            }
+            if (grid[row + 1, col - 1])
+            {
+                neighbors++;
+            }
+            if (grid[row + 1, col])
+            {
+                neighbors++;
+            }
+            if (grid[row + 1, col + 1])
+            {
+                neighbors++;
+            }
+            return neighbors;
+        }
+        public List<List<bool>> ToList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEdge(int row, int col)
+        {
+            if (row == Row-1 || row == 0 || col == Col-1 || col == 0)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public void Tick()
+        {
+            int row = Row;
+            int col = Col;
+            bool[,] newGrid = new bool[row, col];
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    if (IsEdge(i, j))
+                    {
+                        newGrid[i, j] = grid[i, j];
+                    }
+                    else
+                    {
+                        int neighbors = GetAliveNeighbors(i, j);
+                        if (grid[i, j])
+                        {
+                            if (neighbors < 2 || neighbors > 3)
+                            {
+                                newGrid[i, j] = false;
+                            }
+                            else
+                            {
+                                newGrid[i, j] = true;
+                            }
+                        }
+                        else if (!grid[i, j])
+                        {
+                            if (neighbors == 3)
+                            {
+                                newGrid[i, j] = true;
+                            }
+                            else
+                            {
+                                newGrid[i, j] = false;
+                            }
+                        }
+                    }
+                }
+            }
+            grid = newGrid;
+        }
+    }
+}
---
+++ GameTest/CellsTest.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConwaysGameOfLife;
+
+namespace GameTest
+{
+    [TestClass]
+    public class CellsTest
+    {
+        [TestMethod]
+        public void GameExists()
+        {
+            GameOfLife game = new GameOfLife();
+            Assert.IsNotNull(game);
+        }
+        [TestMethod]
+        public void GameWithArgumentsExists()
+        {
+            GameOfLife game = new GameOfLife(2, 2);
+            Assert.IsNotNull(game);
+        }
+        [TestMethod]
+        public void CanCreateGrids()
+        {
+            GameOfLife game = new GameOfLife(3, 3);
+            Assert.AreEqual(9, game.grid.Length);
+        }
+        [TestMethod]
+        public void CellsStartDead()
+        {
+            GameOfLife game = new GameOfLife(3, 3);
+            Assert.IsFalse(game.grid[1,1]);
+        }
+        public void CellsCanChangeState()
+        {
+            GameOfLife game = new GameOfLife(3, 3);
+            game.grid[1, 1] = true;
+            Assert.IsTrue(game.grid[1, 1]);
+        }
+        [TestMethod]
+        public void CountNeighbors()
+        {
+            GameOfLife game = new GameOfLife(3, 3);
+            game.grid[0, 0] = true;
+            Assert.AreEqual(1, game.GetAliveNeighbors(1, 1));
+        }
+        [TestMethod]
+        public void EdgeCase()
+        {
+            GameOfLife game = new GameOfLife(3, 3);
+            Assert.IsTrue(game.IsEdge(0,0));
+            Assert.IsTrue(game.IsEdge(2, 2));
+            Assert.IsTrue(game.IsEdge(0, 2));
+            Assert.IsTrue(game.IsEdge(2, 0));
+        }
+
+        [TestMethod]
+        public void Rule1()
+        //1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+        {
+            GameOfLife game = new GameOfLife(3, 3);
+            game.grid[1, 1] = true;
+            game.Tick();
+            Assert.IsFalse(game.grid[1, 1]);
+        }
+
+        [TestMethod]
+        public void Rule1OnLargerBoard()
+        //1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+        {
+            GameOfLife game = new GameOfLife(8, 8);
+            game.grid[1, 1] = true;
+            game.grid[5, 5] = true;
+            game.Tick();
+            Assert.IsFalse(game.grid[1, 1]);
+            Assert.IsFalse(game.grid[5, 5]);
+        }
+        [TestMethod]
+        public void Blinker()
+        {
+            GameOfLife game = new GameOfLife(5, 5);
+            game.grid[1, 2] = true;
+            game.grid[2, 2] = true;
+            game.grid[3, 2] = true;
+            game.Tick();
+            Assert.IsFalse(game.grid[1, 2]);
+            Assert.IsFalse(game.grid[3, 2]);
+            Assert.IsTrue(game.grid[2, 2]);
+        }
+
+        [TestMethod]
+        public void Toad()
+        {
+            GameOfLife game = new GameOfLife(6, 6);
+            game.grid[2, 2] = true;
+            game.grid[2, 3] = true;
+            game.grid[2, 4] = true;
+            game.grid[3, 1] = true;
+            game.grid[3, 2] = true;
+            game.grid[3, 3] = true;
+            game.Tick();
+            Assert.IsFalse(game.grid[2, 2]);
+            Assert.IsFalse(game.grid[2, 3]);
+            Assert.IsFalse(game.grid[3, 2]);
+            Assert.IsFalse(game.grid[3, 3]);
+            Assert.IsTrue(game.grid[1, 3]);
+            Assert.IsTrue(game.grid[2, 1]);
+            Assert.IsTrue(game.grid[2, 4]);
+            Assert.IsTrue(game.grid[3, 1]);
+            Assert.IsTrue(game.grid[3, 4]);
+            Assert.IsTrue(game.grid[4, 2]);
+        }
+
+    }
+}
+
+
